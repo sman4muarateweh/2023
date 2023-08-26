954ae7271cc43ac5a00daf0d6237a2cdc2b33e5d
SMAN 4 Muara Teweh <94159177+sman4muarateweh@users.noreply.github.com>
2023-08-26 22:09:50 +0700

    Update process_vote.php

--- process_vote.php
+++ process_vote.php
@@ -1,5 +1,4 @@
 <?php
-// Koneksi ke database (gantilah dengan informasi koneksi Anda)
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -10,12 +9,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
-
-// Ambil data calonID yang dikirim melalui POST
 if (isset($_POST['calonID'])) {
     $calonID = $_POST['calonID'];
-
-    // Perbarui jumlah suara untuk calon yang dipilih
     $sql = "UPDATE calon_osis SET jumlah_suara = jumlah_suara + 1 WHERE id_calon = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $calonID);
@@ -25,9 +20,7 @@ if (isset($_POST['calonID'])) {
     } else {
         echo "error";
     }
-
     $stmt->close();
 }
-
 $conn->close();
 ?>
